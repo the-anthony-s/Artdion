5932f13c724e8eafe9a3961d20867a76068f215f
the-anthony-s <antonsergeyev@me.com>
2021-03-13 15:56:55 -0500

    action mailer default url for heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Devise config
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'artdion.herokuapp.com' }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
